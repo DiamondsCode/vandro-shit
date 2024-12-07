73ef75ebc6a56fc6ec609ec8c31b1c0559f817fe
Josh <80289665+DiamondsCode@users.noreply.github.com>
2024-12-06 23:46:03 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,23 +13,23 @@ page = st.sidebar.radio("Go to section:", ["Section 1", "Section 2", "Section 3"
 if page == "Section 1":
     st.image("https://picsum.photos/800/400", caption="describe the photo by text here", use_column_width=True)
     st.write("""
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.11111111111
     """)
 
 elif page == "Section 2":
     st.image("https://picsum.photos/800/401", caption="describe the photo by text here", use_column_width=True)
     st.write("""
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.222222222222
     """)
 
 elif page == "Section 3":
     st.image("https://picsum.photos/800/402", caption="describe the photo by text here", use_column_width=True)
     st.write("""
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.33333333333
     """)
 
 elif page == "Section 4":
     st.image("https://picsum.photos/800/403", caption="describe the photo by text here", use_column_width=True)
     st.write("""
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.444444444444444
     """)
