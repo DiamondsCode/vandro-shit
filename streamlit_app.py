3c8775b3a84fe3fa1f8541350f70b7c8cb3d5209
Josh <80289665+DiamondsCode@users.noreply.github.com>
2024-12-06 23:42:06 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,23 +6,23 @@ st.write("""
     write a description here or some shit
 """)
 
-st.image("biden-medium.png", caption="describe the photo by text here", use_column_width=True, height=450)
+st.image("biden-medium.png", caption="describe the photo by text here", use_column_width=True)
 st.write("""
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
 """)
 
-st.image("biden-medium.png", caption="describe the photo by text here", use_column_width=True, height=450)
+st.image("biden-medium.png", caption="describe the photo by text here", use_column_width=True)
 st.write("""
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
 """)
 
 
-st.image("biden-medium.png", caption="describe the photo by text here", use_column_width=True, height=450)
+st.image("biden-medium.png", caption="describe the photo by text here", use_column_width=True)
 st.write("""
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
 """)
 
-st.image("biden-medium.png", caption="describe the photo by text here", use_column_width=True, height=450)
+st.image("biden-medium.png", caption="describe the photo by text here", use_column_width=True)
 st.write("""
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
 """)
