59b4fe6fddde382fb30aee3df55fa0b162e35143
Josh <80289665+DiamondsCode@users.noreply.github.com>
2024-12-06 23:43:13 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,36 @@
 import streamlit as st
 
+# Set the title of the Streamlit app
 st.title("YouTube Statistics")
 
-st.write("""
-    write a description here or some shit
-""")
+# Sidebar navigation
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Go to section:", ["Section 1", "Section 2", "Section 3", "Section 4"])
 
-st.image("biden-medium.png", caption="describe the photo by text here", use_column_width=True)
-st.write("""
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
-""")
+# Section 1
+if page == "Section 1":
+    st.image("biden-medium.png", caption="describe the photo by text here", use_column_width=True, height=450)
+    st.write("""
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
+    """)
 
-st.image("biden-medium.png", caption="describe the photo by text here", use_column_width=True)
-st.write("""
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
-""")
+# Section 2
+elif page == "Section 2":
+    st.image("biden-medium.png", caption="describe the photo by text here", use_column_width=True, height=450)
+    st.write("""
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
+    """)
 
+# Section 3
+elif page == "Section 3":
+    st.image("biden-medium.png", caption="describe the photo by text here", use_column_width=True, height=450)
+    st.write("""
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
+    """)
 
-st.image("biden-medium.png", caption="describe the photo by text here", use_column_width=True)
-st.write("""
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
-""")
-
-st.image("biden-medium.png", caption="describe the photo by text here", use_column_width=True)
-st.write("""
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
-""")
+# Section 4
+elif page == "Section 4":
+    st.image("biden-medium.png", caption="describe the photo by text here", use_column_width=True, height=450)
+    st.write("""
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
+    """)
